b1fc5ead75976fbbe8f9d1fc13dd4e7cccc029e8
unknown <aluno@.(none)>
2010-03-22 20:10:23 -0300

    clocamos if e erro_reporting

--- mail_form.php
+++ mail_form.php
@@ -1,8 +1,14 @@
 <?php
+	error_reporting (0);
 	$email='joaosteing@gmail.com';
 	$assunto=$_POST['assunto'];
 	$mensagem=$_POST['mensagem'];
 
-	mail ($email, $assunto, $mensagem);
+	if (mail ($email, $assunto, $mensagem)):
 
 ?>
+
+<p>deu certo!!! sucesso!!!! ok!!!</p>
+<?php else:  ?>
+<p>errou</p>
+<?php endif;  ?>
