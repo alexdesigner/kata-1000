3d83b82e4e8f056f49c1567deb490192b78916f9
unknown <aluno@.(none)>
2010-03-22 20:06:53 -0300

    colocamos o post

--- mail_form.php
+++ mail_form.php
@@ -1,7 +1,7 @@
 <?php
-	$email='alexdesigner@gmail.com';
-	$assunto='assunto: bla bla';
-	$mensagem='mensagem: huhuuh';
+	$email='joaosteing@gmail.com';
+	$assunto=$_POST['assunto'];
+	$mensagem=$_POST['mensagem'];
 
 	mail ($email, $assunto, $mensagem);
 
